6ee3b09aa40ae1b07a195de6c1c97655abb428da
tonape123 <tonapenamrata@gmail.com>
2022-08-17 16:16:31 +0530

    commit

---
+++ python programs22/p4.py
@@ -0,0 +1,9 @@
+#how to print multiple Arguments in python and call a function to display the arguments
+def num(a,b,c):
+    print("the value of ",a,"the value of ",b,"the value of ",c)
+
+name="namrata"
+age="24"
+qualification="BE"
+num(name,age,qualification)    
+
