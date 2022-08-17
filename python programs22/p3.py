e5202f313b91a470c98b2c3bb27afa85340c95ed
tonape123 <tonapenamrata@gmail.com>
2022-08-17 16:16:14 +0530

    commit

---
+++ python programs22/p3.py
@@ -0,0 +1,4 @@
+#write a program to sort unsorted numbers using list in ascending order
+sorted_numbers = sorted([20,30,6,8,10,55,72,83,95,78,2,4,1])
+
+print("Sorted in ascending order: ", sorted_numbers)
